0d64d025f83e4025dd94f5dee09ce1d682cb2476
Timea2 <timeapodrug@gmail.com>
2023-05-14 17:55:01 +0200

    added

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <section className="home section" id="home">
+            <div className="home_container container grid">
+                <div className="home_content grid">
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Home
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home_social">
+            <a href="https://www.instagram.com/timea_podrug/" className="home_social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/timea-podrug-30a910216/" className="home_social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/Timea2" className="home_social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+
+    )
+}
+export default Social
