4db65b72355ea48abb54e34a39a7bbc2045c9ca2
Timea2 <timeapodrug@gmail.com>
2023-05-16 16:29:57 +0200

    Skills component
    frontend and backend part done

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -3,8 +3,40 @@ import React from "react";
 
 const Backend = () => {
     return (
-        <div></div>
-       
+        <div className="skills_content">
+
+            <h3 className="skills_title">Backend Developer</h3>
+
+            <div className="skills_box">
+                <div className="skills_group">
+                    <div className="skills_data">
+                    <i class="uil uil-check"></i>
+                    <div>
+                        <h3 className="skills_name">PHP</h3>
+                        <span className="skills_level">Basic</span>
+                    </div>
+                    </div>
+
+                    <div className="skills_data">
+                    <i class="uil uil-check"></i>
+                    <div>
+                        <h3 className="skills_name">Python</h3>
+                        <span className="skills_level">Intermediate</span>
+                    </div>
+                    </div>
+
+                    {/* <div className="skills_data">
+                    <i class="uil uil-check"></i>
+                    <div>
+                        <h3 className="skills_name">C++</h3>
+                        <span className="skills_level">Basic</span>
+                    </div>
+                    </div> */}
+                </div>
+
+              
+            </div>
+        </div>
        
 
 
