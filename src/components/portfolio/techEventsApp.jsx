beb0e8a8c835f2d38ca4b02d9f05e52d7dea1a96
Timea2 <timeapodrug@gmail.com>
2023-06-02 08:20:44 +0200

    next video

--- src/components/portfolio/techEventsApp.jsx
+++ src/components/portfolio/techEventsApp.jsx
@@ -5,18 +5,15 @@ import Slika from "../../assets/next2.jpg"
 
 
 
-
 const Next = () => {
     return (
         <div className="portfolio_container container grid">
-            <div className="portfolio_content">
-
-                <iframe className="portfolio_video" width="560" height="315" src="https://www.youtube.com/embed/dAdb2X_GRxs?loop=1&autoplay=1&playlist=dAdb2X_GRxs" title="YouTube video player" frameborder="0" allow=" accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
-            </div>
-            <div className="portfolio_data portfolio_content portfolio_description" >
+            
+            <div className="portfolio_data">
+                <p className="subtitle">NEXT</p>
+                
+                <br></br>
                 <p className="portfolio_description">
-                    <p className="subtitle">NEXT</p>
                     Cilj konferencije je pokazati mladima mogućnosti i zajednicu koju Split i Hrvatska imaju. <br></br><br></br>
 
                     <span id="date"> 25. OŽUJKA SPLIT DOM MLADIH </span><br></br><br></br>
@@ -30,6 +27,7 @@ const Next = () => {
                 </a>
 
             </div>
+            <img alt="#" src={Slika} className="portfolio_img" />
         </div>
 
 
