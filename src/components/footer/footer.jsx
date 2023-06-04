deec4b18aae15e4d8677df5a14dd12e2cccf151e
Timea2 <timeapodrug@gmail.com>
2023-06-04 16:01:21 +0200

    small text changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer_container container">
-                <h1 className="footer_title">Timea</h1>
+                <h1 className="footer_title">Portfolio</h1>
 
                 <ul className="footer_list">
                     <li>
